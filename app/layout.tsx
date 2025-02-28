94df8f8c1850e483821587052ebf2a9872d81cd3
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-03-01 00:05:28 +0100

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import ClientLayout from "@/components/client-layout";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/contexts/auth-context";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "PokéFusion - Create Unique Pokémon Combinations",
@@ -32,7 +33,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NGJ8SWC8');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className="min-h-screen" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGJ8SWC8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ClerkProvider>
           <AuthProvider>
             <ThemeProvider attribute="class" defaultTheme="light" enableSystem>

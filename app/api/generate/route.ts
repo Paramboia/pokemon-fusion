a0dd4c310f3e3d04a2c88814df42157f21909568
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-03-05 15:57:38 +0100

    update prompt

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -209,7 +209,7 @@ export async function POST(req: Request) {
         image_2: processedImage2,
         control_image: processedImage1,
         merge_mode: "left_right", // Options: full, left_right, up_down, center_square
-        prompt: `a fusion of ${pokemon1Name} and ${pokemon2Name} in a single new Pokemon, clean Pokémon-style illustration with a pure white background, solid white background, game concept art, animation or video game character design, with smooth shading, soft lighting, and a balanced color palette, friendly animation style, completely white background with no black or gray, transparent background`,
+        prompt: `a fusion of ${pokemon1Name} and ${pokemon2Name} in a single new Pokemon, clean Pokémon-style illustration with a pure white background, solid white background, game concept art, animation or video game character design, with smooth shading, soft lighting, and a balanced color palette, friendly animation style, kid friendly style, completely white background with no black or gray, transparent background`,
         negative_prompt: "blurry, realistic, 3D, distorted, messy, uncanny, color background, garish, soft, ugly, broken, distorted, futuristic, render, digital, black background, dark background, dark color palette, dark shading, dark lighting, any background other than white",
         upscale_2x: true // Enable upscaling for better quality
       };

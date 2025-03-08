bd72b1ac5ad4ea27cb2e6c3c83b28e593ed1f39b
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-03-08 23:11:45 +0100

    issue google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,20 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NQ57560QHC"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-NQ57560QHC');
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />

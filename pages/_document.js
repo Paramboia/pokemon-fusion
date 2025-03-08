a92d61ac2380eb87c9aa549337562fd3330ffcef
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-03-08 23:04:48 +0100

    added the Google tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NQ57560QHC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NQ57560QHC');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

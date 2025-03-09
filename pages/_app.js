156f28980b8161382bd346768667c8d027867af0
Miguel Joao Macedo Parente <miguel.joao.parente@outlook.com>
2025-03-09 14:44:43 +0100

    issue GA tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,17 @@
 import '../app/globals.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   
   useEffect(() => {
-    // Google Analytics
+    // Track page views on route change
     const handleRouteChange = (url) => {
-      window.gtag('config', 'G-NQ57560QHC', {
-        page_path: url,
-      });
+      gtag.pageview(url);
     };
-
-    // Initialize gtag on mount
-    const script1 = document.createElement('script');
-    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-NQ57560QHC';
-    script1.async = true;
-    document.head.appendChild(script1);
-
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-NQ57560QHC');
-    `;
-    document.head.appendChild(script2);
-
-    // Track route changes
+    
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
